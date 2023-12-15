0ee597e4bc640d51aa1b7e7f7a3005215565879d
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-16 00:19:39 +0530

    Add auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,62 @@
+const authController = require('express').Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const dotenv = require('dotenv').config()
+
+// register
+authController.post('/register', async(req,res) => {
+    try{
+        const {username, email, password} = req.body;
+
+        const isExisting = await User.findOne({email: req.body.email})
+        if(isExisting){
+            throw new Error("Already such an account with this email. Try with new one")
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        return res.status(201).json({message: "User regisyered successfully"})
+
+    } catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+// login
+authController.post('/login', async(req,res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({email: email})
+        if(!user){
+            throw new Error("User credentials are wrong")
+        }
+
+        const comparePassword = await bcrypt.compare(password, user.password)
+        if(!comparePassword){
+            throw new Error("User credentials are wrong")
+        }
+
+        const token = jwt.sign(
+            {id: user.id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '5h'}
+        )
+
+        return res.status(201).json({ token })
+
+    } catch(error) {
+        return res.status(500).json(error.message)
+    }
+})
+
+
+module.exports = authController
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
-const app = express()
+const authController = require('./controllers/authController')
 
+const app = express()
 
 // connect db
 mongoose
@@ -17,6 +18,16 @@ mongoose
     console.error('Error connecting to DB:', error);
   });
 
+
+// server is on port 5000. client is on port 3000. We are going to get a cors error!. But cors() removes that error
+app.use(cors())
+
+// those two middlewares mae req.body accessible, otherwise it would be undefined
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// routes
+app.use('/auth', authController)
 
 // start server
 app.listen(process.env.PORT, () => console.log(`Server has been started in ${process.env.PORT} successfully`))
