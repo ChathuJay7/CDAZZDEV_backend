66d4d340d81e2e4345daaab2e61d2937748dfe5a
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-16 11:28:30 +0530

    Add updatePost method

--- controllers/postController.js
+++ controllers/postController.js
@@ -32,4 +32,37 @@ postController.post('/', async(req,res) => {
     }
 })
 
+// update post
+postController.put('/:postId', async (req, res) => {
+    try {
+        const { title, description, userId } = req.body;
+        const postId = req.params.postId;
+
+        // Check if the post exists
+        const existingPost = await Post.findById(postId);
+        if (!existingPost) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
+        // Check if at least one field is provided for update
+        if (!title && !description && !userId) {
+            return res.status(400).json({ message: 'At least one field is required for update' });
+        }
+
+        // Update only the provided fields
+        if (title) existingPost.title = title;
+        if (description) existingPost.description = description;
+
+        // Save the updated post
+        await existingPost.save();
+
+        return res.status(200).json({ message: 'Post updated successfully' });
+    } catch (error) {
+        console.error('Error updating post:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
+
 module.exports = postController
