ba4e4ebc28a452fbfc3525e3e5011d9f82fac863
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-16 11:45:23 +0530

    Add get single method

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,6 +14,25 @@ postController.get('/', async(req, res) => {
 })
 
 
+// get post by id
+postController.get('/:postId', async (req, res) => {
+    try {
+        const postId = req.params.postId;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
+        return res.status(200).json({post});
+    } catch (error) {
+        console.error('Error getting post by ID:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
 // create post
 postController.post('/', async(req,res) => {
     try{
@@ -32,28 +51,25 @@ postController.post('/', async(req,res) => {
     }
 })
 
+
 // update post
 postController.put('/:postId', async (req, res) => {
     try {
         const { title, description, userId } = req.body;
         const postId = req.params.postId;
 
-        // Check if the post exists
         const existingPost = await Post.findById(postId);
         if (!existingPost) {
             return res.status(404).json({ message: 'Post not found' });
         }
 
-        // Check if at least one field is provided for update
         if (!title && !description && !userId) {
             return res.status(400).json({ message: 'At least one field is required for update' });
         }
 
-        // Update only the provided fields
         if (title) existingPost.title = title;
         if (description) existingPost.description = description;
 
-        // Save the updated post
         await existingPost.save();
 
         return res.status(200).json({ message: 'Post updated successfully' });
